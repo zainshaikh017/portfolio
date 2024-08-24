890d4d6230b602bdea162edb5bbfc874c5f9a9d2
zain shaikh <139116312+zainshaikh017@users.noreply.github.com>
2024-08-24 16:43:13 +0500

    cv add

--- lib/model/Persnol_information.dart
+++ lib/model/Persnol_information.dart
@@ -1,16 +1,17 @@
 class PersnolInformation {
-  static const String cv_link = "https://drive.google.com/file/d/1YlXvPffh_cn4jAEI3idBNPLqFOnkt1Em/view?usp=sharing";
+  static const String cv_link =
+      "https://drive.google.com/file/d/13rJ7yYv5GPISDfWHehs_JhUpKJy1fWaC/view?usp=sharing";
   static const String name = "Zain Shaikh";
   static const String image = "assets/images/profile.jpeg";
   static const String title = "Fluetter Developer";
   static const String contect = "03113023018";
-  static  const String whatsapp = "https://wa.me/$contect";
+  static const String whatsapp = "https://wa.me/$contect";
   static const String email = "zain77569@gmail.com";
-  static const String linkIn = "https://www.linkedin.com/in/zain-shaikh-52302726b?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app";
+  static const String linkIn =
+      "https://www.linkedin.com/in/zain-shaikh-52302726b?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app";
   static const String github = "https://github.com/zainshaikh017";
   static const String twitter = "";
   static const String indeed = "";
   static const String linkInUser = "zain-shaikh";
   static const String githubUser = "@zainshaikh017";
-
 }
